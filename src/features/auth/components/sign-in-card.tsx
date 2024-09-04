fbb11281a5d9c83471794a38578b4d4354c24db7
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-09-04 15:15:50 -0300

    feat: import Separator

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,5 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 export const SignInCard = () => {
-  return <div>SignInCard</div>;
+  return (
+    <Card className="size-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Login to continue</CardTitle>
+        <CardDescription>Use your email or another service</CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value={""}
+            onChange={() => {}}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={false}
+            value={""}
+            onChange={() => {}}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size={"lg"} disabled={false}>
+            Continue
+          </Button>
+        </form>
+        <Separator />
+      </CardContent>
+    </Card>
+  );
 };
